b478d881c0b305b6522e6bd742ee6187d5082366
Scott Gordon <5c077.60rd0n@gmail.com>
2024-06-19 14:22:31 -0700

    Add index.js to video-streaming directory

---
+++ video-streaming/src/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const fs = require("fs");
+
+if (!process.env.PORT) {
+    throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
+}
+
+const PORT = process.env.PORT;
+const app = express();
+
+app.get("/video", async (req, res) => {
+    const videoPath = "./videos/SampleVideo_1280x720_1mb.mp4";
+    const stats = await fs.promises.stat(videoPath);
+
+    res.writeHead(200, {
+        "Content-Length": stats.size,
+        "Content-Type": "video/mp4",
+    });
+    fs.createReadStream(videoPath).pipe(res);
+});
+
+app.listen(PORT, () => {
+    console.log(`Microservice online`);
+})
