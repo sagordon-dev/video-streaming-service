d7dacf90919a4ae80d77bfe42068f21710d7f18e
Scott Gordon <5c077.60rd0n@gmail.com>
2024-06-22 05:04:51 -0700

    Add logic to video-streaming to utilize mongodb

--- video-streaming/src/index.js
+++ video-streaming/src/index.js
@@ -1,27 +1,50 @@
 const express = require("express");
 const http = require("http");
+const mongodb = require("mongodb");
 
 const PORT = process.env.PORT;
 const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST;
 const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_PORT);
+const DBHOST = process.env.DBHOST;
+const DBNAME = process.env.DBNAME;
 
-const app = express();
 
-app.get("/video", (req, res) => {
-    const forwardRequest = http.request(
-        {
-            host: VIDEO_STORAGE_HOST,
-            port: VIDEO_STORAGE_PORT,
-            path: '/video?path=SampleVideo_1280x720_1mb.mp4',
-            method: 'GET',
-            headers: req.headers
-        },
-        forwardResponse => {
-            res.writeHeader(forwardResponse.statusCode, forwardResponse.headers);
-            forwardResponse.pipe(res);
+async function main() {
+    const clinet = await mongodb.MongoClient.connect(DBHOST);
+    const db = client.db(DBNAME);
+    const videosCollection = db.collection("videos");
+
+
+    const app = express();
+
+    app.get("/video", async (req, res) => {
+        const videoId = new mongodb.ObjectId(req.query.id);
+        const videoRecord = await videosCollection.findOne({ _id: videoId })
+        if (!videoRecord) {
+            res.sendStatus(404);
+            return;
         }
-    );
-    req.pipe(forwardRequest);
-});
 
-app.listen(PORT);
+        const forwardRequest = http.request(
+            {
+                host: VIDEO_STORAGE_HOST,
+                port: VIDEO_STORAGE_PORT,
+                path: `/video?path=${videoRecord.videoPath}`,
+                method: 'GET',
+                headers: req.headers
+            },
+            forwardResponse => {
+                res.writeHeader(forwardResponse.statusCode, forwardResponse.headers);
+                forwardResponse.pipe(res);
+            }
+        );
+        req.pipe(forwardRequest);
+    });
+    app.listen(PORT);
+}
+
+main()
+    .catch(err => {
+        console.error("Microservices failed to start.");
+        console.error(err && err.stack || err);
+    });
