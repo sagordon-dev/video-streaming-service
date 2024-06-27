54b5fefda7f2a175822a0615400e162b33167fe3
Scott Gordon <5c077.60rd0n@gmail.com>
2024-06-26 17:53:34 -0700

    Update video-streaming index.js to use history message and dumb down video streaming while testing

--- video-streaming/src/index.js
+++ video-streaming/src/index.js
@@ -1,83 +1,64 @@
 const express = require("express");
+const fs = require("fs");
 const http = require("http");
-const mongodb = require("mongodb");
-
-//
-// Throws an error if the any required environment variables are missing.
-//
 
 if (!process.env.PORT) {
     throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
 }
 
-if (!process.env.VIDEO_STORAGE_HOST) {
-    throw new Error("Please specify the host name for the video storage microservice in variable VIDEO_STORAGE_HOST.");
-}
-
-if (!process.env.VIDEO_STORAGE_PORT) {
-    throw new Error("Please specify the port number for the video storage microservice in variable VIDEO_STORAGE_PORT.");
-}
-
-if (!process.env.DBHOST) {
-    throw new Error("Please specify the databse host using environment variable DBHOST.");
-}
-
-if (!process.env.DBNAME) {
-    throw new Error("Please specify the name of the database using environment variable DBNAME");
-}
-
-//
-// Extracts environment variables to globals for convenience.
-//
-
 const PORT = process.env.PORT;
-const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST;
-const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_PORT);
-const DBHOST = process.env.DBHOST;
-const DBNAME = process.env.DBNAME;
 
-console.log(`Forwarding video requests to ${VIDEO_STORAGE_HOST}:${VIDEO_STORAGE_PORT}.`);
+function sendViewedMessage(videoPath) {
+    const postOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "applicaton/json",
+        },
+    };
 
-async function main() {
-    const client = await mongodb.MongoClient.connect(DBHOST); // Connects to the database.
-    const db = client.db(DBNAME);
-    const videosCollection = db.collection("videos");
-    
-    const app = express();
-        
-    app.get("/video", async (req, res) => {
-        const videoId = new mongodb.ObjectId(req.query.id);
-        const videoRecord = await videosCollection.findOne({ _id: videoId });
-        if (!videoRecord) {
-            // The video was not found.
-            res.sendStatus(404);
-            return;
-        }
+    const requestBody = {
+        videoPath: videoPath
+    };
 
-        console.log(`Translated id ${videoId} to path ${videoRecord.videoPath}.`);
+    const req = http.request(
+        "http://history/viewed",
+        postOptions
+    );
 
-        const forwardRequest = http.request( // Forward the request to the video storage microservice.
-            {
-                host: VIDEO_STORAGE_HOST,
-                port: VIDEO_STORAGE_PORT,
-                path:`/video?path=${videoRecord.videoPath}`, // Video path now retrieved from the database.
-                method: 'GET',
-                headers: req.headers
-            }, 
-            forwardResponse => {
-                res.writeHeader(forwardResponse.statusCode, forwardResponse.headers);
-                forwardResponse.pipe(res);
-            }
-        );
-        
-        req.pipe(forwardRequest);
+    req.on("close", () => {
+        console.log("Sent 'viewed' message to history microservice.");
     });
 
-    //
-    // Starts the HTTP server.
-    //
+    req.on("error", (err) => {
+        console.error("Failed to send 'viewed' message!");
+        console.error(err && err.stack || err);
+    });
+
+    req.write(JSON.stringify(requestBody));
+    req.end();
+}
+
+
+async function main() {
+
+    const app = express();
+
+    app.get("/video", async (req, res) => {
+        const videoPath = "./videos/SampleVideo_1280x720_1mb.mp4";
+        const stats = await fs.promises.stat(videoPath);
+
+        res.writeHead(200, {
+            "Content-Length": stats.size,
+            "Content-Type": "video/mp4"
+        });
+
+        fs.createReadStream(videoPath).pipe(res);
+
+        sendViewedMessage(videoPath);
+    });
+
     app.listen(PORT, () => {
-        console.log(`Microservice listening, please load the data file db-fixture/videos.json into your database before testing this microservice.`);
+        console.log("Microservices online.");
     });
 }
 
