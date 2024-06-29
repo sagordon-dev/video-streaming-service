5f08139b2f2af94428d6d7ba22e14ff9c8fb3bfa
Scott Gordon <5c077.60rd0n@gmail.com>
2024-06-29 13:55:31 -0700

    Add rabbitmq logic into streaming video microservice

--- video-streaming/src/index copy.js
+++ video-streaming/src/index copy.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const fs = require("fs");
-const amqp = require("amqplib");
+const amqp = require('amqplib');
 
 if (!process.env.PORT) {
     throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
@@ -13,30 +13,34 @@ if (!process.env.RABBIT) {
 const PORT = process.env.PORT;
 const RABBIT = process.env.RABBIT;
 
+//
 // Application entry point.
+//
 async function main() {
 
     console.log(`Connecting to RabbitMQ server at ${RABBIT}.`);
 
-    const messagingConnection = await amqp.connect(RABBIT);
+    const messagingConnection = await amqp.connect(RABBIT); // Connects to the RabbitMQ server.
 
     console.log("Connected to RabbitMQ.");
 
-    const messageChannel = await messagingConnection.createChannel();
-
-    // Send the "viewed" to the history microservice.
-
-    function sendViewedMessage(messageChannel, videoPath) {
-        console.log(`Publishing message on "viewed" queue.`);
-
-        const msg = { videoPath: videoPath };
-        const jsonMsg = JSON.stringify(msg);
-        messageChannel.publish("", "viewed", Buffer.from(jsonMsg));
-    }
+    const messageChannel = await messagingConnection.createChannel(); // Creates a RabbitMQ messaging channel.
+    
+	//
+	// Send the "viewed" to the history microservice.
+	//
+	function sendViewedMessage(messageChannel, videoPath) {
+	    console.log(`Publishing message on "viewed" queue.`);
+	
+	    const msg = { videoPath: videoPath };
+	    const jsonMsg = JSON.stringify(msg);
+	    messageChannel.publish("", "viewed", Buffer.from(jsonMsg)); // Publishes message to the "viewed" queue.
+	}
 
     const app = express();
 
-    app.get("/video", async (req, res) => {
+    app.get("/video", async (req, res) => { // Route for streaming video.
+
         const videoPath = "./videos/SampleVideo_1280x720_1mb.mp4";
         const stats = await fs.promises.stat(videoPath);
 
@@ -44,10 +48,10 @@ async function main() {
             "Content-Length": stats.size,
             "Content-Type": "video/mp4",
         });
-
+    
         fs.createReadStream(videoPath).pipe(res);
 
-        sendViewedMessage(messageChannel, videoPath);
+        sendViewedMessage(messageChannel, videoPath); // Sends the "viewed" message to indicate this video has been watched.
     });
 
     app.listen(PORT, () => {
